4e667bf407fc2537aabc1f20dfb30713824a4ef3
lemax97 <lemax97@mail.ru>
2020-08-31 15:41:07 +0300

    Tutorial game - Asteroids clone

---
+++ core/src/main/java/SpaceRocks/SpaceRocksGame.java
@@ -0,0 +1,10 @@
+package SpaceRocks;
+
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class SpaceRocksGame extends BaseGame {
+    @Override
+    public void create() {
+
+    }
+}
---
+++ lwjgl3/src/main/java/SpaceRocks/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,24 @@
+package SpaceRocks.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import SpaceRocks.SpaceRocksGame;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+	public static void main(String[] args) {
+		createApplication();
+	}
+
+	private static Lwjgl3Application createApplication() {
+		return new Lwjgl3Application(new SpaceRocksGame(), getDefaultConfiguration());
+	}
+
+	private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+		Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+		configuration.setTitle("Space Rocks");
+		configuration.setWindowedMode(800, 600);
+		configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+		return configuration;
+	}
+}
